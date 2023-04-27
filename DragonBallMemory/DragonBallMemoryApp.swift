9f670d3c034c5145c503366160c1fe17fda9ecb2
Francesco Merola <fmerola19@gmail.com>
2023-04-27 19:01:17 +0200

    Initial Commit

---
+++ DragonBallMemory/DragonBallMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonBallMemoryApp.swift
+//  DragonBallMemory
+//
+//  Created by Francesco Merola on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DragonBallMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
